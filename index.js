85c9ed024ba518f6936dbe27b4cc70ea44208ad1
CaritolaDiosadelOlimpo <carolina.medina@usach.cl>
2020-03-28 20:05:43 -0300

    installing dependencies and creating callback

---
+++ index.js
@@ -0,0 +1,9 @@
+//installing dependencies 
+const cheerio = require('cheerio');
+const fs      = require('fs');
+const request = require('request');
+
+//creating callback
+request('https://www.santander.cl/', (err, resp, body)=> {
+
+});
