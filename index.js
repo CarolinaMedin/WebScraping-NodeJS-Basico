c32a76174f5bcba9a531739d6acfcf03e60f6de1
CaritolaDiosadelOlimpo <carolina.medina@usach.cl>
2020-03-28 22:49:16 -0300

    adding dependencies and code

--- index.js
+++ index.js
@@ -3,7 +3,21 @@ const cheerio = require('cheerio');
 const fs      = require('fs');
 const request = require('request');
 
+const albunes = [];
 //creating callback
-request('https://www.santander.cl/', (err, resp, body)=> {
+request('https://www.sodastereo.com/albums/', (err, resp, body)=> {
+    
+    if(!err && resp.statusCode == 200) // if there is no error and it works correctly
+        let $ = cheerio.load(body);
+        $('h2.album_name', '#main').each(function (){
+            var nameAlbum = $(this).attr('h2');
+            if(nameAlbum.indexOf(class.album_name)!= -1){
+                albunes.push(nameAlbum)
+            }
+ 
+        })
 
 });
+
+// cheerio uses the $ as a signo
+// "#" used to analyze in contex
